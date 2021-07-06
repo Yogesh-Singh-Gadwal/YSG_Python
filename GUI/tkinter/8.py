43e58b40b50e0025a3661a988f87550d8e7fb7b2
Yogesh Singh Gadwal <g.yogeshsingh@gmail.com>
2021-07-07 00:40:15 +0530

    Create 8.py

---
+++ GUI/tkinter/8.py
@@ -0,0 +1,14 @@
+import tkinter 
+
+from tkinter import messagebox
+
+top = tkinter.Tk()
+top.geometry("350x350")
+
+def m1():
+   messagebox.showinfo( "Micky mouse", "Disney World")
+
+B = tkinter.Button(top, text = "Micky", command = m1)
+B.place(x = 50,y = 50)
+
+top.mainloop()
