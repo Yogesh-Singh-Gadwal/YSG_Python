c23f51b4d7754846a9580192eb99d017cb10b5a4
Yogesh Singh Gadwal <g.yogeshsingh@gmail.com>
2021-07-07 00:53:37 +0530

    Create 28.py

---
+++ GUI/tkinter/28.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import tkinter
+
+top = Tk()
+
+Lb1 = Listbox(top)
+Lb1.insert(1, "Micky")
+Lb1.insert(2, "ATS")
+Lb1.insert(3, "INDIA")
+Lb1.insert(4, "PYTHON")
+Lb1.insert(5, "STUDENTS")
+Lb1.insert(6, "SUCCESS")
+Lb1.pack()
+
+top.mainloop()
+
