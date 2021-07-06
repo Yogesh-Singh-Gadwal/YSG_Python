31b0a8ff1d016dcb0fb31faa372a89a5c49033d8
Yogesh Singh Gadwal <g.yogeshsingh@gmail.com>
2021-07-07 00:30:28 +0530

    Create 5.py

---
+++ GUI/tkinter/5.py
@@ -0,0 +1,9 @@
+import tkinter
+window = tkinter.Tk()
+window.title("ATH")
+window.geometry('350x200')
+lbl = tkinter.Label(window, text = "Micky Data")
+lbl.grid(column =0, row = 2)
+btn = tkinter.Button(window, text = "Button-1")
+btn.grid(column =1, row =0)
+window.mainloop()
