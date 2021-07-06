1c8c879ca7ec1d38eace2ba3f76643500f2fcef5
Yogesh Singh Gadwal <g.yogeshsingh@gmail.com>
2021-07-07 00:27:26 +0530

    Create 3.py

---
+++ GUI/tkinter/3.py
@@ -0,0 +1,6 @@
+import tkinter
+window = tkinter.Tk()
+window.title("ATH")
+lbl = tkinter.Label(window, text = "Micky..", font = ("French ScriptMT", 50))
+lbl.grid(column =0, row = 0)
+window.mainloop()
