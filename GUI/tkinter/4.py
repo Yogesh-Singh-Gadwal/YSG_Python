3458102d9feed673fd3c93d297e1684e5cc3c6ae
Yogesh Singh Gadwal <g.yogeshsingh@gmail.com>
2021-07-07 00:28:27 +0530

    Create 4.py

---
+++ GUI/tkinter/4.py
@@ -0,0 +1,7 @@
+import tkinter
+window = tkinter.Tk()
+window.title("ATH")
+lbl = tkinter.Label(window, text = "Micky..", font = ("French ScriptMT", 50))
+lbl.grid(column =0, row = 0)
+window.geometry('550x200')
+window.mainloop()
