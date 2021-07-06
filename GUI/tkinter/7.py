daeb98a4eaefbfc8714bd62ef1a2da8a57e470b7
Yogesh Singh Gadwal <g.yogeshsingh@gmail.com>
2021-07-07 00:32:35 +0530

    Create 7.py

---
+++ GUI/tkinter/7.py
@@ -0,0 +1,10 @@
+import tkinter
+window = tkinter.Tk()
+window.title("ATH")
+window.geometry('350x200')
+lbl = tkinter.Label(window, text = "Micky Data")
+lbl.grid(column =0, row = 0)
+btn = tkinter.Button(window, text = "Button-1", bg = "lightblue", fg = "green")
+btn.grid(column =1, row =0)
+btn.place(x = 50, y = 50)
+window.mainloop()
